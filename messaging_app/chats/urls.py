5cf349fa6d5aaee29cb6dcca91e7696662a7720c
John Muhura <jowamumjwesh@gmail.com>
2025-07-20 18:00:43 +0300

    Build api endpoints with views checkspass1

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+import rest_framework.routers as routers
 from . import views
 
 # Create a router and register our viewsets with it.
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', views.ConversationViewSet, basename='conversation')
 router.register(r'messages', views.MessageViewSet, basename='message')
 
